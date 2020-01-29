bc7c1996af0fffab016d665c8ed4eeb1a6e68c9d
Kully <adam@plot.ly>
2020-01-29 00:56:08 -0500

    rotate triangle functions

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "lib.h"
 #include "math.h"
+#include "stdio.h"
 
+// colors
 #define RED 0xffff0000
 #define ORANGE 0xffffa500
 #define GREEN 0xff00ff00
@@ -30,9 +32,18 @@ int main(void)
         lock();
         clear();
 
-        put((int) (xres/2 + 30*sin(n)), 8, RED);
+        float x0 = (xres/2 + 30*sin(n));
+        float y0 = (yres/2 - 30*cos(n));
 
-        n += 0.06;
+        float x1 = (xres/2 + 30*sin(n+(2*M_PI)/3));
+        float y1 = (yres/2 - 30*cos(n+(2*M_PI)/3));
+
+        float x2 = (xres/2 + 30*sin(n+(4*M_PI)/3));
+        float y2 = (yres/2 - 30*cos(n+(4*M_PI)/3));
+
+        draw_triangle(x0, y0, x1, y1, x2, y2, TEAL);
+
+        n += 0.02;
         unlock();
         delay(15);
     }
